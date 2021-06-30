fec2920681f2e2e54ea24a9cc98c65924358f6df
Dhruvish09 <pateldhruvish612000@gmail.com>
2021-06-30 10:46:54 +0530

    update

--- store/views/Signin.py
+++ store/views/Signin.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from store.models.customer import Customer
 from django.contrib.auth.hashers import check_password, make_password
 from django.views import View
+from django.contrib.auth import authenticate, login
+from django.contrib.sessions.models import Session
 
 class Signin(View):
 
@@ -10,19 +12,32 @@ class Signin(View):
         return render(request, 'signin.html')
     
     def post(self , request):
-
         email = request.POST.get('email')
         password = request.POST.get('password')
         customer = Customer.get_customer_by_email(email)
-        error_message = None
-        if customer:
+        try:
             flag = check_password(password , customer.password)
-            if flag:
-                return redirect('index')
-            else:
-                error_message = 'Email or Password is Invalid!'
-        
-        else:
-            error_message = 'Email or Password Invalid!'
+            login(request , flag)
+            request.session['email'] = email
+            return redirect('index')
+        except:
+            return render(request,'index.html')
+         
 
-        return render(request , 'signin.html', {'error' : error_message})
+        # email = request.POST.get('email')
+        # password = request.POST.get('password')
+        # customer = Customer.get_customer_by_email(email)
+        # error_message = None
+        # if customer:
+        #     flag = check_password(password , customer.password)
+        #     if flag:
+        #         login(request )
+        #         request.session['email'] = email
+        #         return redirect('index')
+        #     else:
+        #         error_message = 'Email or Password is Invalid!'
+        
+        # else:
+        #     error_message = 'Email or Password Invalid!'
+
+        # return render(request , 'signin.html', {'error' : error_message})
