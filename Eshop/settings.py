91dca7779b700285afdd75a06afddf3750774d94
Patel Dhruvish <pateldhruvish612000@gmail.com>
2024-05-05 16:24:19 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eshopdb',
+        'HOST': 'database-1.cmm3ewlz88z2.ap-south-1.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': 'dhruv612000',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
